b50bb9e3af3d70588a6118c407f61a8e5d6d6ebb
nopicts <nopporn@iapp.co.th>
2018-08-30 10:04:10 +0700

    setup configuration with next + apollo

--- next.config.js
+++ next.config.js
@@ -3,24 +3,7 @@ const glob = require('glob')
 
 module.exports = {
     webpack(config, options) {
-        const { dir, defaultLoaders } = options
-
-        // config.pageExtensions.push(".ts", ".tsx")
-
-        config.resolve.extensions.push(".ts", ".tsx")
-
         config.module.rules.push(
-            {
-                test: /\.(ts|tsx)/,
-                include: [dir],
-                exclude: /node_modules/,
-                use: [
-                    defaultLoaders.babel,
-                    {
-                        loader: "awesome-typescript-loader",
-                    }
-                ]
-            },
             {
                 test: /\.(css|scss)/,
                 loader: 'emit-file-loader',
